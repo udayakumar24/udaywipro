93f6b300e87ca7fc230fd7c64c532b55faf8e965
udayakumar24 <udhya2001@gmail.com>
2024-05-16 17:17:38 +0530

    change

--- File1.java
+++ File1.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     int x=10
+	    int y=200
+	    public void add(){
+		    System.out.println(x+y);
+	    }
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("world");
